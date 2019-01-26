df1181244d866b1c5657da0a5c106a6d4a7840a7
TychoLaptop\iMrBa_000 <TychoZaal@gmail.com>
2019-01-26 15:31:52 +0100

    Singelton added

---
+++ Global Game Jam 2019/Assets/CharacteristicsAndData.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacteristicsAndData : MonoBehaviour
+{
+    private static CharacteristicsAndData _instance;
+
+    public static CharacteristicsAndData Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<CharacteristicsAndData>();
+            }
+
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public enum Pets
+    {
+        CAT,
+        DOG
+    }
+
+    public enum Characteristic
+    {
+        HUNGRY,
+        MESSY,
+        OUTSIDE,
+        MOODY
+    }
+
+    public string name;
+    public Pets favPet;
+    public Characteristic characteristic;
+    public int date;
+
+    public void Sleep()
+    {
+        date++;
+    }
+}
