bbe5e7225832c0c1c182fd176ab844978aec1492
Durin Kompier <durinfacebook@hotmail.com>
2019-01-26 02:27:20 +0100

    - Made interactible objects light up after looking at them - Added a dot to the camera

---
+++ Global Game Jam 2019/Assets/Scripts/CameraRaycast.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRaycast : MonoBehaviour
+{
+    private GameObject target;
+    public RaycastHit raycastHit;
+    public Collider raycastHitCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        int layerMask = 1 << 9;
+        
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+
+            //Do the thing please
+            hit.transform.gameObject.GetComponent<InteractableObject>().shineCounter++;
+        }
+    }
+}
---
+++ Global Game Jam 2019/Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    public float interactionRange;
+    public float glowIntensity = 1;
+    public float shineCounter, previousShineCounter;
+    public float shineCounterLimit = 10;
+    public bool onlyInteractWhenCrouching;
+    public bool beingLookedAt;
+    private Material objectMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("ResetCheck");
+        objectMaterial = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BalanceTheShine();
+        CheckForEmission();
+    }
+
+    private void BalanceTheShine()
+    {
+        if(shineCounter < 0)
+        {
+            shineCounter = 0;
+        }
+
+        if(shineCounter > shineCounterLimit)
+        {
+            shineCounter = shineCounterLimit;
+        }
+    }
+     
+    IEnumerator ResetCheck()
+    {
+        while(true)
+        {
+            Debug.Log("Boop");
+            previousShineCounter = shineCounter;
+            yield return new WaitForSeconds(3f);
+            if (previousShineCounter == shineCounterLimit)
+            {
+                shineCounter--;
+            }
+            else if (previousShineCounter == shineCounter)
+            {
+                shineCounter = 0;
+            }
+        }
+    }
+    //Checks whether the player has looked at the object for long enough
+    void CheckForEmission()
+    {
+        if (shineCounter >= shineCounterLimit)
+        {
+            ShowEmission();
+        }
+        else
+        {
+            HideEmission();
+        }
+    }
+
+    //Makes the object glow
+    public void ShowEmission()
+    {
+        objectMaterial.SetFloat("_EmissionStrength", glowIntensity);
+    }
+
+    //Stops the object from glowing
+    void HideEmission()
+    {
+        objectMaterial.SetFloat("_EmissionStrength", 0);
+    }
+}
